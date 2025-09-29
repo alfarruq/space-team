41443a758888f847688718c37ae254fd96d41cca
alfarruq <mengboyevfarrux01@gmail.com>
2025-09-29 17:10:59 +0500

    changed header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,21 @@
 
 import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpg'
 import './style.css'
 
 
 function Header() {
     return (
         <header className='container' >
-            <a href="/"> <img src="" alt="" /></a>
+            <a href="/"> <img className='logo' src={logo} alt="" /></a>
             <ul>
                 <li><Link to={'/'} >Bosh Sahifa</Link></li>
                 <li><Link to={'/courses'} >Kurslar</Link></li>
                 <li><Link to={'/tests'} >Tests</Link></li>
                 <li><Link to={'/blog'} >Blog</Link></li>
                 <li><Link to={'/'} >Biz haqimizda</Link></li>
-                <li><button>Kirish</button></li>
-                <li><button>Ro'yxatdan o'tish</button></li>
+                <li><button className='login' >Kirish</button></li>
+                <li><button  className='signup' >Ro'yxatdan o'tish</button></li>
             </ul>
         </header>
     );
