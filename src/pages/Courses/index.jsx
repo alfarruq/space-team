6efbc3b863c65426d381c4900a4bdc129820c80a
alfarruq <mengboyevfarrux01@gmail.com>
2025-10-08 17:08:53 +0500

    courses

--- src/pages/Courses/index.jsx
+++ src/pages/Courses/index.jsx
@@ -1,11 +1,49 @@
 
 
-
+import { useEffect, useState } from 'react';
+import './style.css'
 
 function Course() {
-    return ( 
-        <div></div>
-     );
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+
+        async function get() {
+            let response = await fetch('https://fastapi-course-app.onrender.com/categories/')
+            let data = await response.json()
+
+            setData(data)
+        }
+        get()
+
+    }, [])
+
+    console.log(data);
+
+
+    return (
+        <div className="container" >
+            <h1>Kurslar</h1>
+
+            <div className="courses">
+                {
+                    data?.map((kurs) => {
+                        return (
+                            <div key={kurs.id} className="course">
+                                <img src={'https://fastapi-course-app.onrender.com' + kurs?.image} alt={kurs.name} />
+                                <h3  >{kurs.name}</h3>
+                                <p>{kurs.description}</p>
+                                <button>Kursni ko'rish</button>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+
+        </div>
+    );
 }
 
 export default Course;
